993b2e8bbf8d66672263c58fe7f8a44403809caf
Luis Manyari <lmanyari@hotmail.com>
2024-03-18 10:57:49 -0400

    Category Buttons :P~

--- little-lemon/CategoryList.js
+++ little-lemon/CategoryList.js
@@ -22,9 +22,10 @@ const CategoryList = ({ categories }) => {
                 <Pressable
                     key={category} // use category as key if it's unique
                     style={({ pressed }) => ({
-                        backgroundColor: pressed || selectedCategories.includes(category) ? '#333333' : '#FBDABB',
+                        backgroundColor: pressed || selectedCategories.includes(category) ? '#333333' : '#EDEFEE',
                         padding: 10,
                         margin: 10,
+                        borderRadius: 15,
                     })}
                     onPress={() => toggleCategory(category)}
                 >
